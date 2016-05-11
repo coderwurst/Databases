1e5f528ab28a24d69a1a78f4a5deaefe6378fa0f
coderwurst <coderwurst@gmail.com>
2016-05-11 12:15:34 +0100

    Queries, Prepared Statements and Stored Procedures

---
+++ UDemy SQL/src/test/java/com/coderwurst/method/test/JDBCTest.java
@@ -0,0 +1,42 @@
+package com.coderwurst.method.test;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCTest {
+	
+	public static void main (String [] args) {
+		
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		try {
+			// 1. connect to DB
+			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "student", "student");
+			
+			System.out.println("DB connection succesful >>>");
+			
+			// 2. Create a statement
+			myStmt = myConn.createStatement();
+			
+			// 3. Execute SQL Query
+			myRs = myStmt.executeQuery("select * from employees");
+			
+			// 4. Process Result set
+			while (myRs.next()) {
+				System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+
+}
